18503ceac406b290ebde0506ef85856aa9bede0b
ezraalejandre <159743115+ezraalejandre@users.noreply.github.com>
2024-04-17 19:29:57 +0800

    Create testing.cpp
    Ideas, Pseudocode

---
+++ testing.cpp
@@ -0,0 +1,44 @@
+/*
+Group Number 14
+Group Leader: Ezra L. Alejandre
+Group Member: 
+Ric Manuel C. Avilla
+Gezelle Franz D. Maranan
+Topic: Console Based Asylum Management System
+*/
+
+//(Ezra) hi, idea pa lang ilagay dito. kung may gusto kayo idagdag, comments at suggestions lagay lang kayo tas lagay nyo rin pangalan nyo salamattttttttttt 
+
+
+/* (Ezra) idea 1:
+Churvaness Asylum Virtual Registration
+kunin muna information ng user, parang log in: e-mail, name, age, contact no., address.
+sa umpisa may main menu.
+tapos may options, pagpipilian ay diagnosed/undiagnosed.
+pag undiagnosed, online consultation muna sya sa idea 2, magfill up ng appointment form para magpaevaluate muna sya sa psychiatrist onsite sa ating asylum.
+pag diagnosed, may options, pagpipilian nakalagay mga sinearch na mental disorders hahahaa balew
+        nakagroup na yung mga mental disorders base sa matino, medj matino, medj valew, valew na tlga(research tungkol dito)
+        tapos base sa kung gaano kalala mental disorder niya, dun sya ilalagay kung saan sya nararapat na group.
+*/
+
+/* (Ezra) idea 2:
+Churvaness Asylum Virtual Consultation
+kunin ule information kung di pa nakukuha.
+main menu. pili ule kung diagnosed/undiagnosed.
+diagnosed: punta sa registration.
+undiagnosed: mag aanswer ang user ng questionnaire(research tungkol sa tinatanong sa mga valew).
+        kung ano yung nagmamatch na mental disorder sa kanya base sa mga tanong, yun yung magiging result na mental disorder.
+pagkalabas ng result, may option kung gusto niya magregister sa ating asylum/ayaw.
+        pag ayaw, tapos na.
+        pag magpaparegister, pupunta sa idea 1 virtual registration.
+*/
+
+
+
+
+
+
+/*
+PSEUDOCODE:
+
+*/
